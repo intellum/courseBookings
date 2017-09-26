4700865e136fa6c9f4e62efce05b56a40c4852b2
Daryl Hedley <darylhedley@hotmail.com>
2017-09-26 07:00:40 +0100

    Updated Bloom API calls

--- frontend/index.js
+++ frontend/index.js
@@ -11,8 +11,8 @@ import BookedCourseBookingsReducer from './reducers/bookedCourseBookingsReducer'
 registerMenuItem({text: 'Course Bookings', _link: "courseBookings", _icon: "calendar-text", _permission: "admin"}, 0);
 registerComponent('courseBookings', 'Course Bookings', CourseBookingsContainer, 'dashboard:learner');
 registerComponent('bookedCourseBookings', 'Booked Course Bookings', BookedCourseBookingsContainer, 'myLearning');
-registerRoute({path: 'courseBookings', component: CourseBookingsAdminContainer});
-registerRoute({path: 'courseBookings/:id', component: EditCourseBookingContainer});
+registerRoute('courseBookings', CourseBookingsAdminContainer);
+registerRoute('courseBookings/:id', EditCourseBookingContainer);
 registerReducer('courseBookings', CourseBookingsReducer);
 registerReducer('courseBooking', CourseBookingReducer);
 registerReducer('courseBookingsDashboard', CourseBookingDashboardReducer);
