2b264397cfb544429e642ed060160d9cb55c80de
Joe <joe.langley@appitierre.com>
2019-06-13 11:50:04 +0100

    Updated accessibility of course bookings

--- frontend/components/bookedCourseBookings.js
+++ frontend/components/bookedCourseBookings.js
@@ -60,17 +60,13 @@ var BookedCourseBookings = React.createClass({
     render: function() {
 
         return (
-            <div 
-                className="booked-course-bookings"
-                tabIndex="0"
-                aria-label="Course bookings. Here is a selection of the your course bookings."
-            >
+            <section className="booked-course-bookings" aria-label={LP('myLearning', 'courseBookingsSectionAriaLabel', 'sentencecase')}>
                 <div className="booked-course-bookings-title">
                     {LP('courseBookings', 'youBooked', null)}
                 </div>
                 {this.renderItems()}
                 
-            </div>
+            </section>
         );
     }
 
